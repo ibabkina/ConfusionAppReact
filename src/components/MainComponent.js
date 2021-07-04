fa306ed22b6f7f4efe4274aad14b8087a1deb01b
ibabkina <i.babkina700@gmail.com>
2021-07-04 10:08:07 -0400

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent'; // removed ./components cause already in component folder
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes'; //go one level up to src first ..
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // Making MainComponent to be a container component
 class Main extends Component {
@@ -16,27 +18,27 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-}
+
 
   render() {
+
+    const HomePage = () => {
+        return(
+            <Home />
+        );
+    }
       return (
       <div>
         <Header />
-        <Menu dishes={ this.state.dishes }
-                onClick={(dishId) => this.onDishSelect(dishId)} />  
-                {/* in onClick() I only pass dishId cause I have DISHES here and can grab info and change parameter 
-                dish to dishId as well */}
-                {/* We passed onClick as a property in MenuComponent */}
-        <Dishdetail dishSelected={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]}/>
-        {/* This arrow functions what it does is it helps to select out all those dishes for which 
-        the dishId matches the selectedDish. it returns an array. We have to select the first item in the array [0]*/}
-      <Footer />
+        <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
